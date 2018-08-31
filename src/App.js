33452019441fb9d927deae5f1a699386db1c47e0
tb <tbussanich@gmail.com>
2018-08-31 16:49:27 -0700

    initial file setup and text animation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Container">
+        <p className="Marquee">
+          Test Test Test
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
