88f53d87fa4d0e263c028dfa2e64c8d3e30a2d6f
tb <tbussanich@gmail.com>
2018-08-31 21:49:43 -0700

    create main component and move code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Main from './Main';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="Container">
-        <p className="Marquee">
-          Test Test Test
-        </p>
+      <div>
+        <Main />
       </div>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Container">
+        <div className="Marquee">
+          <p>oh, hey.</p>
+
+          <p>fancy seeing you here.</p>
+
+          <p>i know things are hard right now.</p>
+
+          <p>Vestibulum eleifend elementum est, non porttitor elit sollicitudin sed. Sed auctor id risus tincidunt blandit. Vestibulum ornare feugiat est, vel iaculis lorem congue non. Nam hendrerit nibh et orci rutrum faucibus eu tempus elit. Aenean hendrerit volutpat ex sit amet dictum. Etiam pulvinar leo ac metus bibendum, non efficitur ex molestie. Nullam sit amet purus augue. Quisque vel nisi vitae dolor scelerisque volutpat. Duis viverra mauris et leo efficitur aliquet. Duis ipsum odio, lobortis nec efficitur ac, sagittis sed sem. Nullam sed ligula non massa euismod tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam lectus felis, cursus vel accumsan eu, egestas ac elit. Praesent eu ante maximus tellus mollis imperdiet. Nullam convallis leo nibh, ut lacinia mi lacinia eget.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
